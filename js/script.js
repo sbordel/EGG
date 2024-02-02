aba687b1344e148989bf3b85e5c12fda8d042da1
sam_ <45014739+sbordel@users.noreply.github.com>
2024-02-02 11:49:42 -0500

    fixed egg animation

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ let mbRecipe;
 
 
 $(document).ready(function () {
-  // $.getJSON("json/recipes.json", function (data) {
-  $.getJSON("../EGG/json/recipes.json", function (data) {
+  $.getJSON("json/recipes.json", function (data) {
+  // $.getJSON("../EGG/json/recipes.json", function (data) {
     dataFromJSON = data;
     for (let j = 0; j < dataFromJSON.length; j++) {
     }
